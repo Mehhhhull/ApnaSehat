035adfa2ce1b8e9c259822df5d2970008aab901e
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-08-26 21:52:04 +0530

    DB configure and Model

---
+++ lib/db.ts
@@ -0,0 +1,37 @@
+import mongoose,{ Mongoose} from "mongoose";
+
+
+const MONGO_URI=process.env.MONGO_URI!;
+
+interface MongooseConn{
+    conn:Mongoose|null;
+    promise:Promise<Mongoose>|null;
+}
+
+let cached:MongooseConn=(global as any).mongoose;
+
+if(!cached){
+    cached=(global as any).mongoose={
+        conn:null,
+        promise:null
+    }
+}
+    
+
+const connect = async ()=>{
+    if(cached.conn){
+        return cached.conn;
+    }
+
+    cached.promise=cached.promise||mongoose.connect(MONGO_URI,{
+        dbName:"apnasehat",
+        bufferCommands:false,
+        connectTimeoutMS:10000,
+    })
+
+    cached.conn=await cached.promise;
+
+    return cached.conn;
+}
+
+export default connect;
